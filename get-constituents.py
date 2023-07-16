22c26546c332d74fd12bff082196aa20a93dd346
Jun Sun <yfiua@qq.com>
2023-07-16 14:57:15 +0200

    update

--- get-constituents.py
+++ get-constituents.py
@@ -9,7 +9,7 @@ from selectorlib import Extractor
 def convert_symbol(symbol):
     return symbol + 'SS' if symbol[0] == '6' else symbol + 'SZ'
 
-def get_constituents_csi300():
+def get_constituents_from_wglh(url):
     selector_yml = '''
                     Symbol:
                         css: 'td small.text-secondary'
@@ -25,7 +25,6 @@ def get_constituents_csi300():
 
     e = Extractor.from_yaml_string(selector_yml)
 
-    url = 'https://wglh.com/ChinaIndicesCon/SH000300/'
     headers = { 'User-Agent' : 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36' }
     r = requests.get(url, headers=headers)
 
@@ -36,8 +35,12 @@ def get_constituents_csi300():
 
     return df
 
+def get_constituents_csi300():
+    url = 'https://wglh.com/ChinaIndicesCon/SH000300/'
+    return get_constituents_from_wglh(url)
+
 if __name__ == '__main__':
     df = get_constituents_csi300()
-    df.to_csv('data/constituents-csi300.csv', index=False)
-    df.to_json('data/constituents-csi300.json', orient='records')
+    df.to_csv('docs/constituents-csi300.csv', index=False)
+    df.to_json('docs/constituents-csi300.json', orient='records')
 
