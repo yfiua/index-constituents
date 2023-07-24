f79a43f03b1723875d3402bb90fa5ac13b94c8c5
Jun Sun <yfiua@qq.com>
2023-07-24 22:57:02 +0200

    add hsi, dax, ftse100

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # index-constituents
 Get the constituents of popular stock indices.
+All symbols are consistent with those in [Yahoo Finance](https://finance.yahoo.com/).
 
 ## Data source
-[乌龟量化](https://wglh.com/)
-[Slickcharts](https://www.slickcharts.com/)
+* [乌龟量化](https://wglh.com/)
+* [Slickcharts](https://www.slickcharts.com/)
+* [Bloomberg](https://www.bloomberg.com/)
--- get-constituents.py
+++ get-constituents.py
@@ -89,6 +89,103 @@ def get_constituents_dowjones():
     url = 'https://www.slickcharts.com/dowjones'
     return get_constituents_from_slickcharts(url)
 
+# DAX
+def get_constituents_dax():
+    # convert symbol from 'SYMBOL:GR' to 'SYMBOL.DE'
+    def convert_symbol_dax(symbol):
+        return symbol[:-3] + '.DE'
+
+    selector_yml = '''
+                    Symbol:
+                        css: 'div.security-summary a.security-summary__ticker'
+                        xpath: null
+                        multiple: true
+                        type: Text
+                    Name:
+                        css: 'div.security-summary a.security-summary__name'
+                        xpath: null
+                        multiple: true
+                        type: Text
+                   '''
+
+    e = Extractor.from_yaml_string(selector_yml)
+
+    url = 'https://www.bloomberg.com/quote/DAX:IND/members'
+    headers = { 'User-Agent' : 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36' }
+    r = requests.get(url, headers=headers)
+
+    data = e.extract(r.text)
+    df = pd.DataFrame(data)
+
+    df['Symbol'] = df['Symbol'].apply(convert_symbol_dax)
+
+    return df
+
+# Hang Seng Index
+def get_constituents_hsi():
+    # convert symbol from 'XX:HK' to '00XX.HK'
+    def convert_symbol_hsi(symbol):
+        return symbol.rjust(7, '0').replace(':', '.')
+
+    selector_yml = '''
+                    Symbol:
+                        css: 'div.security-summary a.security-summary__ticker'
+                        xpath: null
+                        multiple: true
+                        type: Text
+                    Name:
+                        css: 'div.security-summary a.security-summary__name'
+                        xpath: null
+                        multiple: true
+                        type: Text
+                   '''
+
+    e = Extractor.from_yaml_string(selector_yml)
+
+    url = 'https://www.bloomberg.com/quote/HSI:IND/members'
+    headers = { 'User-Agent' : 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36' }
+    r = requests.get(url, headers=headers)
+
+    data = e.extract(r.text)
+    df = pd.DataFrame(data)
+
+    df['Symbol'] = df['Symbol'].apply(convert_symbol_hsi)
+
+    return df
+
+# FTSE 100 (UKX)
+def get_constituents_ftse100():
+    # convert symbol from 'SYMBOL:LN' to 'SYMBOL.L'
+    def convert_symbol_ftse100(symbol):
+        return symbol.replace(':LN', '.L')
+
+    selector_yml = '''
+                    Symbol:
+                        css: 'div.security-summary a.security-summary__ticker'
+                        xpath: null
+                        multiple: true
+                        type: Text
+                    Name:
+                        css: 'div.security-summary a.security-summary__name'
+                        xpath: null
+                        multiple: true
+                        type: Text
+                   '''
+
+    e = Extractor.from_yaml_string(selector_yml)
+
+    url = 'https://www.bloomberg.com/quote/UKX:IND/members'
+    headers = { 'User-Agent' : 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36' }
+    r = requests.get(url, headers=headers)
+
+    data = e.extract(r.text)
+    df = pd.DataFrame(data)
+
+    df['Symbol'] = df['Symbol'].apply(convert_symbol_ftse100)
+
+    return df
+
+# main
 if __name__ == '__main__':
     print('Fetching the constituents of CSI 300...')
     try:
@@ -138,5 +235,28 @@ if __name__ == '__main__':
     except:
         print('Failed to fetch the constituents of Dow Jones.')
 
-    print('Done.')
+    print('Fetching the constituents of DAX...')
+    try:
+        df = get_constituents_dax()
+        df.to_csv('docs/constituents-dax.csv', index=False)
+        df.to_json('docs/constituents-dax.json', orient='records')
+    except:
+        print('Failed to fetch the constituents of DAX.')
 
+    print('Fetching the constituents of Hang Seng Index...')
+    try:
+        df = get_constituents_hsi()
+        df.to_csv('docs/constituents-hsi.csv', index=False)
+        df.to_json('docs/constituents-hsi.json', orient='records')
+    except:
+        print('Failed to fetch the constituents of Hang Seng Index.')
+
+    print('Fetching the constituents of FTSE 100...')
+    try:
+        df = get_constituents_ftse100()
+        df.to_csv('docs/constituents-ftse100.csv', index=False)
+        df.to_json('docs/constituents-ftse100.json', orient='records')
+    except:
+        print('Failed to fetch the constituents of FTSE 100.')
+
+    print('Done.')
