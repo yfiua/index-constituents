a0521393dce6ff702a344651fa215e2cbfd78e05
Jun S <yfiua6@gmail.com>
2025-04-19 23:20:22 +0200

    replace deprecated data source

--- get-constituents.py
+++ get-constituents.py
@@ -6,32 +6,26 @@ import requests
 
 from selectorlib import Extractor
 
-def convert_symbol_wglh(symbol):
-    return symbol + 'SS' if symbol[0] == '6' else symbol + 'SZ'
+def get_constituents_from_csindex(url):
+    # convert symbol from 'SYMBOL' to 'SYMBOL.SZ' or 'SYMBOL.SS'
+    def convert_symbol_csindex(symbol):
+        match symbol[0]:
+            case '0' | '3':
+                return symbol + '.SZ'
+            case '6':
+                return symbol + '.SS'
+            case '4' | '8':
+                return symbol + '.BJ'
 
-def get_constituents_from_wglh(url):
-    selector_yml = '''
-                    Symbol:
-                        css: 'td small.text-secondary'
-                        xpath: null
-                        multiple: true
-                        type: Text
-                    Name:
-                        css: 'tr td:nth-of-type(2) a'
-                        xpath: null
-                        multiple: true
-                        type: Text
-                   '''
+        return symbol
 
-    e = Extractor.from_yaml_string(selector_yml)
+    # read the excel file from the url
+    df = pd.read_excel(url, dtype=str)
 
-    headers = { 'User-Agent' : 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36' }
-    r = requests.get(url, headers=headers)
+    df = df[['成份券代码Constituent Code', '成份券名称Constituent Name']]
+    df.columns = ['Symbol', 'Name']
 
-    data = e.extract(r.text)
-    df = pd.DataFrame(data)
-
-    df['Symbol'] = df['Symbol'].apply(convert_symbol_wglh)
+    df['Symbol'] = df['Symbol'].apply(convert_symbol_csindex)
 
     return df
 
@@ -61,28 +55,37 @@ def get_constituents_from_slickcharts(url):
 
 # 沪深300
 def get_constituents_csi300():
-    url = 'https://wglh.com/ChinaIndicesCon/SH000300/'
-    return get_constituents_from_wglh(url)
+    url = 'https://oss-ch.csindex.com.cn/static/html/csindex/public/uploads/file/autofile/cons/000300cons.xls'
+    return get_constituents_from_csindex(url)
 
 # 中证500
 def get_constituents_csi500():
-    url = 'https://wglh.com/ChinaIndicesCon/SH000905/'
-    return get_constituents_from_wglh(url)
+    url = 'https://oss-ch.csindex.com.cn/static/html/csindex/public/uploads/file/autofile/cons/000500cons.xls'
+    return get_constituents_from_csindex(url)
 
 # 中证1000
 def get_constituents_csi1000():
-    url = 'https://wglh.com/ChinaIndicesCon/SH000852/'
-    return get_constituents_from_wglh(url)
+    url = 'https://oss-ch.csindex.com.cn/static/html/csindex/public/uploads/file/autofile/cons/000852cons.xls'
+    return get_constituents_from_csindex(url)
 
 # 上证指数
 def get_constituents_sse():
-    url = 'https://wglh.com/ChinaIndicesCon/SH000001/'
-    return get_constituents_from_wglh(url)
+    url = 'https://oss-ch.csindex.com.cn/static/html/csindex/public/uploads/file/autofile/cons/000001cons.xls'
+    return get_constituents_from_csindex(url)
 
 # 深证成指
 def get_constituents_szse():
-    url = 'https://wglh.com/ChinaIndicesCon/SZ399001/'
-    return get_constituents_from_wglh(url)
+    url = 'https://www.szse.cn/api/report/ShowReport?SHOWTYPE=xls&CATALOGID=1747_zs&ZSDM=399001'
+
+    # read the excel file from the url
+    df = pd.read_excel(url, dtype=str)
+
+    df = df[['证券代码', '证券简称']]
+    df.columns = ['Symbol', 'Name']
+
+    df['Symbol'] = df['Symbol'] + '.SZ'
+
+    return df
 
 # NASDAQ100
 def get_constituents_nasdaq100():
